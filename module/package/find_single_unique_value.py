c0cacaa716c99ec7f74a497b2d5162dc1d0f54e9
ridddhesh <riddhic1817@gmail.com>
2026-01-06 00:40:28 +0530

    till oops polyforms done

---
+++ module/package/find_single_unique_value.py
@@ -0,0 +1,21 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def unique(lst):\n",
+    "    return list(set(lst))"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
